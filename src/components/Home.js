c85aabae4c2cb0c692c3096f4ff47a0b28e519ab
r-ILIASS <iliass.root@gmail.com>
2021-12-22 12:58:33 +0100

    feat: add responsive featured hero image

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 // Components
+import HeroImage from "./HeroImage";
 
 // Hooks
 import { useHomeFetch } from "../hooks/useHomeFetch";
@@ -11,9 +12,20 @@ import NoImage from "../images/no_image.jpg";
 
 const Home = () => {
   const { data, loading, error } = useHomeFetch();
+  const { results } = data;
 
-  console.log("data ", data);
-  return <div>Home Page</div>;
+  console.log(data);
+  return (
+    <>
+      {results[0] ? (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${results[0].backdrop_path}`}
+          title={results[0].original_title}
+          text={results[0].overview}
+        />
+      ) : null}
+    </>
+  );
 };
 
 export default Home;
