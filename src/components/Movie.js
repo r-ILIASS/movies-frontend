a7cb6527a39af8d79cd298dd71ecb1640837f0a9
r-ILIASS <iliass.root@gmail.com>
2021-12-23 15:12:11 +0100

    feat: add Actor component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -7,6 +7,7 @@ import BreadCrumb from "./BreadCrumb";
 import MovieInfo from "./MovieInfo";
 import MovieInfoBar from "./MovieInfoBar";
 import Grid from "./Grid";
+import Actor from "./Actor";
 import Spinner from "./Spinner";
 // Hooks
 import { useMovieFetch } from "../hooks/useMovieFetch";
@@ -26,7 +27,25 @@ const Movie = () => {
     <>
       <BreadCrumb movieTitle={data.original_title} />
       <MovieInfo movie={data} />
-      <MovieInfoBar time={data.runtime} budget={data.budget} revenue={data.revenue} />
+      <MovieInfoBar
+        time={data.runtime}
+        budget={data.budget}
+        revenue={data.revenue}
+      />
+      <Grid header="Actors">
+        {data.actors.map((actor) => (
+          <Actor
+            key={actor.credit_id}
+            name={actor.name}
+            character={actor.character}
+            imageUrl={
+              actor.profile_path
+                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                : NoImage
+            }
+          />
+        ))}
+      </Grid>
     </>
   );
 };
